d7645c012ed6a4fc321a82ddfbcc819547481c39
dave-scar <david.scarimbolo@gmail.comgit config --global user.email david.scarimbolo@gmail.com>
2016-12-01 17:01:04 -0500

    search

--- FlipViceFeed/app/src/main/java/com/joelimyx/flipvicefeed/main/main/MainActivity.java
+++ FlipViceFeed/app/src/main/java/com/joelimyx/flipvicefeed/main/main/MainActivity.java
@@ -1,5 +1,7 @@
 package com.joelimyx.flipvicefeed.main.main;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.annotation.NonNull;
@@ -12,6 +14,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
@@ -32,7 +38,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements MainAdapter.OnItemSelectedListener,
-        NavigationView.OnNavigationItemSelectedListener{
+        NavigationView.OnNavigationItemSelectedListener {
     private RecyclerView mMainRecyclerView;
     private MainAdapter mAdapter;
     private DrawerLayout mDrawerLayout;
@@ -45,14 +51,19 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         mVolleySingleton = VolleySingleton.getInstance(this);
 
+        //Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+
         //RecyclerView
         mMainRecyclerView = (RecyclerView) findViewById(R.id.main_recyclerview);
-        mMainRecyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        mMainRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
 
-        mTwoPane = findViewById(R.id.fragment_container)!=null;
+        mTwoPane = findViewById(R.id.fragment_container) != null;
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open_nav_bar,R.string.close_nav_bar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open_nav_bar, R.string.close_nav_bar);
         mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
@@ -62,7 +73,7 @@ public class MainActivity extends AppCompatActivity
         //Network Connection
         ConnectivityManager manager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = manager.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected()){
+        if (networkInfo != null && networkInfo.isConnected()) {
             String url = "http://www.vice.com/api/getlatest/";
             StringRequest request = new StringRequest(Request.Method.GET, url,
                     new Response.Listener<String>() {
@@ -72,23 +83,23 @@ public class MainActivity extends AppCompatActivity
                             findViewById(R.id.progressbar).setVisibility(View.GONE);
 
                             //Extracting data
-                            GsonArticle gsonArticle = new Gson().fromJson(response,GsonArticle.class);
+                            GsonArticle gsonArticle = new Gson().fromJson(response, GsonArticle.class);
                             List<Item> items = gsonArticle.getData().getItems();
 
                             //Setup up adapter to recycler view
-                            mAdapter = new MainAdapter(items,MainActivity.this,MainActivity.this);
+                            mAdapter = new MainAdapter(items, MainActivity.this, MainActivity.this);
                             mMainRecyclerView.setAdapter(mAdapter);
                         }
-            },
+                    },
                     new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError error) {
                             Toast.makeText(MainActivity.this, "Error getting articles", Toast.LENGTH_SHORT).show();
                         }
-            });
+                    });
             mVolleySingleton.addToRequestQueue(request);
-        }else{
-            Snackbar.make(findViewById(R.id.drawer_layout),"No Network Connection",Snackbar.LENGTH_INDEFINITE).show();
+        } else {
+            Snackbar.make(findViewById(R.id.drawer_layout), "No Network Connection", Snackbar.LENGTH_INDEFINITE).show();
         }
     }
 
@@ -100,15 +111,78 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             // TODO: 11/30/16 Populate switch case
-            case R.id.stuff:
+
+            case R.id.news:
                 findViewById(R.id.progressbar).setVisibility(View.VISIBLE);
-                mAdapter.swapdata("stuff");
+                mAdapter.swapdata("News");
+                findViewById(R.id.progressbar).setVisibility(View.GONE);
+                break;
+            case R.id.tech:
+                findViewById(R.id.progressbar).setVisibility(View.VISIBLE);
+                mAdapter.swapdata("Tech");
+                findViewById(R.id.progressbar).setVisibility(View.GONE);
+                break;
+            case R.id.music:
+                findViewById(R.id.progressbar).setVisibility(View.VISIBLE);
+                mAdapter.swapdata("Music");
+                findViewById(R.id.progressbar).setVisibility(View.GONE);
+                break;
+            case R.id.sports:
+                findViewById(R.id.progressbar).setVisibility(View.VISIBLE);
+                mAdapter.swapdata("Sports");
+                findViewById(R.id.progressbar).setVisibility(View.GONE);
+                break;
+            case R.id.travel:
+                findViewById(R.id.progressbar).setVisibility(View.VISIBLE);
+                mAdapter.swapdata("Travel");
+                findViewById(R.id.progressbar).setVisibility(View.GONE);
+                break;
+            case R.id.fashion:
+                findViewById(R.id.progressbar).setVisibility(View.VISIBLE);
+                mAdapter.swapdata("Fashion");
+                findViewById(R.id.progressbar).setVisibility(View.GONE);
+                break;
+            case R.id.nsfw:
+                findViewById(R.id.progressbar).setVisibility(View.VISIBLE);
+                mAdapter.swapdata("NSFW");
+                findViewById(R.id.progressbar).setVisibility(View.GONE);
+                break;
+            case R.id.food:
+                findViewById(R.id.progressbar).setVisibility(View.VISIBLE);
+                mAdapter.swapdata("Food");
+                findViewById(R.id.progressbar).setVisibility(View.GONE);
+                break;
+            case R.id.culture:
+                findViewById(R.id.progressbar).setVisibility(View.VISIBLE);
+                mAdapter.swapdata("Culture");
                 findViewById(R.id.progressbar).setVisibility(View.GONE);
                 break;
         }
         mDrawerLayout.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    //Inflate Toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    //Select Option on Toolbar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                return true;
+            case R.id.search:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
