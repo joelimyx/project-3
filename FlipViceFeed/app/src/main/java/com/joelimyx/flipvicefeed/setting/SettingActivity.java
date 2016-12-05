a2aa6bf59549d887d0e1351680ed781fa0ae2b9f
Joe Lim <joelimyx@gmail.com>
2016-12-05 15:28:57 -0500

    Sweet Sweet animation

--- FlipViceFeed/app/src/main/java/com/joelimyx/flipvicefeed/setting/SettingActivity.java
+++ FlipViceFeed/app/src/main/java/com/joelimyx/flipvicefeed/setting/SettingActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.joelimyx.flipvicefeed.R;
@@ -32,6 +33,21 @@ public class SettingActivity extends AppCompatActivity {
                     replace(R.id.setting_container,new NotificationFragment())
                     .commit();
         }
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            overridePendingTransition(R.anim.no_animation, R.anim.checkout_scale_out);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.no_animation, R.anim.checkout_scale_out);
     }
 }
